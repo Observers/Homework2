24afe6d82634e2181f21c14fabb5b6f3c1f9d525
Chung-Yen Lu <luderrick@gmail.com>
2020-01-13 00:40:42 +0800

    Role Maintenance - modify role fully implemented.

--- Homework2/Homework2/Controllers/SystemSetupController.cs
+++ Homework2/Homework2/Controllers/SystemSetupController.cs
@@ -51,7 +51,7 @@ namespace Homework2.Controllers
         }
 
         [HttpPost]
-            public ActionResult AddRole(FormCollection form)
+        public ActionResult AddRole(FormCollection form)
         {
             using (Trainee15Entities db = new Trainee15Entities())
             {
@@ -62,11 +62,11 @@ namespace Homework2.Controllers
                 roleAdd.role = form["role"];
                 roleAdd.description = form["description"];
                 var selectStatus = form["selectStatus"];
-                if(selectStatus == "1")
+                if (selectStatus == "1")
                 {
                     roleAdd.status = true;
                 }
-                else 
+                else
                 {
                     roleAdd.status = false;
                 }
@@ -89,15 +89,63 @@ namespace Homework2.Controllers
         }
 
         [HttpPost]
-        public ActionResult ModifyRole()
-        {
-            return View();
-        }
-
-        [HttpGet]
         public ActionResult ModifyRole(FormCollection form)
         {
-            return View();
+            using (Trainee15Entities db = new Trainee15Entities())
+            {
+                var selectRole = form["selectRole"];
+                if (selectRole != null)
+                {
+                    int iSelectRole = int.Parse(selectRole);
+                    Role role = db.Roles.SingleOrDefault(x => x.roleID == iSelectRole);
+                    return View("ModifyRole", role);
+                }
+                else
+                {
+                    var viewModel = new Role();
+                    viewModel.ListA = db.Roles.ToList();
+                    viewModel.ListB = db.Roles.ToList();
+                    return View("RoleMaintenance", viewModel);
+                }
+
+            }
+        }
+
+        [HttpPost]
+        public ActionResult ModifyDatabase(FormCollection form)
+        {
+            using (Trainee15Entities db = new Trainee15Entities())
+            {
+                int userID = (int)Session["userID"];
+                var user = db.Accounts.SingleOrDefault(x => x.userID == userID);
+
+                var selectRole = form["selectRole"];
+                System.Diagnostics.Debug.WriteLine(selectRole);
+                int iSelectRole = int.Parse(selectRole);
+                Role roleModified = db.Roles.SingleOrDefault(x => x.roleID == iSelectRole);
+                roleModified.description = form["description"];
+                var selectStatus = form["selectStatus"];
+                if (selectStatus == "1")
+                {
+                    roleModified.status = true;
+                }
+                else
+                {
+                    roleModified.status = false;
+                }
+                DateTime curretDateTime = DateTime.Now;
+                roleModified.modifyDate = curretDateTime;
+                roleModified.modifyUser = user.username;
+
+                db.SaveChanges();
+
+                var viewModel = new Role();
+                viewModel.ListA = db.Roles.ToList();
+                List<Role> list = new List<Role>();
+                viewModel.ListB = list;
+
+                return View("RoleMaintenance", viewModel);
+            }
         }
 
         [HttpPost]
