db3ba898ef185aa764315eecdad5d30004b09f1c
chung-yen <luderrick@gmail.com>
2020-02-05 14:28:44 +0800

    Updated the implementation of NPOI to role maintenance and menu maintenance.

---
+++ Homework2/Homework2/Static/js/menuMaintenance.js
@@ -0,0 +1,26 @@
+﻿function GetTableValues() {
+    //Create an Array to hold the Table values.
+    var menus = new Array();
+
+    //Reference the Table.
+    var table = document.getElementsByName("table");
+    console.log(table[0].rows.length);
+    //Loop through Table Rows.
+    for (var i = 1; i < table[0].rows.length; i++) {
+        //Reference the Table Row.
+        var row = table[0].rows[i];
+
+        //Copy values from Table Cell to JSON object.
+        var menu = {};
+        menu.menuNo = row.cells[1].innerHTML;
+        menu.level = row.cells[2].innerHTML;
+        menu.title = row.cells[3].innerHTML;
+        menu.linkType = row.cells[4].innerHTML;
+        menu.linkUrl = row.cells[5].innerHTML;
+        menu.status = row.cells[6].innerHTML;
+        menus.push(user);
+    }
+
+    //Convert the JSON object to string and assign to Hidden Field.
+    document.getElementById("JSON").value = JSON.stringify(menus);
+}
---
+++ Homework2/Homework2/Static/js/roleMaintenance.js
@@ -0,0 +1,27 @@
+﻿function GetTableValues() {
+    //Create an Array to hold the Table values.
+    var roles = new Array();
+
+    //Reference the Table.
+    var table = document.getElementsByName("table");
+    console.log(table[0].rows.length);
+    //Loop through Table Rows.
+    for (var i = 1; i < table[0].rows.length; i++) {
+        //Reference the Table Row.
+        var row = table[0].rows[i];
+
+        //Copy values from Table Cell to JSON object.
+        var role = {};
+        role.role = row.cells[1].innerHTML;
+        role.roleDescription = row.cells[2].innerHTML;
+        role.status = row.cells[3].innerHTML;
+        role.createDate = row.cells[4].innerHTML;
+        role.createUser = row.cells[5].innerHTML;
+        role.modifyDate = row.cells[6].innerHTML;
+        role.modifyUser = row.cells[7].innerHTML;
+        roles.push(user);
+    }
+
+    //Convert the JSON object to string and assign to Hidden Field.
+    document.getElementById("JSON").value = JSON.stringify(roles);
+}
